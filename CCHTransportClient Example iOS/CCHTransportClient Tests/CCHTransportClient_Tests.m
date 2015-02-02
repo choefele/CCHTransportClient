06f0c2520f2c36a0e6519a7c5d2ab278c79dec3d
Claus Höfele <choefele@ebay.com>
2015-02-02 16:07:46 +0100

    Add tests

---
+++ CCHTransportClient Example iOS/CCHTransportClient Tests/CCHTransportClient_Tests.m
@@ -0,0 +1,40 @@
+//
+//  CCHTransportClient_Tests.m
+//  CCHTransportClient Tests
+//
+//  Created by Hoefele, Claus on 02.02.15.
+//  Copyright (c) 2015 Claus Höfele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CCHTransportClient_Tests : XCTestCase
+
+@end
+
+@implementation CCHTransportClient_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
