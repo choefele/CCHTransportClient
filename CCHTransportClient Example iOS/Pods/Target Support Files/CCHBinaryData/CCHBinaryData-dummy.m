4e79ac9ad1f47d8cd1a6a032265e379401f8f3ca
Claus Höfele <claus@claushoefele.com>
2015-12-06 18:30:16 +0100

    Update pods
    - CocoaPods 0.36.4 -> 0.39.0
    - CCHBinaryData 1.0.1 -> 1.0.2

---
+++ CCHTransportClient Example iOS/Pods/Target Support Files/CCHBinaryData/CCHBinaryData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CCHBinaryData : NSObject
+@end
+@implementation PodsDummy_CCHBinaryData
+@end
---
+++ CCHTransportClient Example iOS/Pods/Target Support Files/CCHTransportClient/CCHTransportClient-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CCHTransportClient : NSObject
+@end
+@implementation PodsDummy_CCHTransportClient
+@end
